569f80bc2a0eb893f5bbab34a9c256b3cce56d40
Diego Ramirez <diego.ramirez.jove@gmail.com>
2022-03-21 18:44:39 -0400

    Corrección en control LCT - módulo USC

--- SRC/FUN/usc.py
+++ SRC/FUN/usc.py
@@ -9,13 +9,13 @@ def unidad_secuencial_calculo(estado_anterior_USC, entrada_externa_USC, senal_co
     dato_externo = entrada_externa_USC
 
     control_ALU = senal_control_USC[0:12]
-    control_LCT = senal_control_USC[12:21]
+    control_LCT = senal_control_USC[12:22]
 
     entrada_A = acumulador_A_anterior
     entrada_B = dato_externo
 
     resultado_actual, banderas_actual = unidad_aritmetica_logica(entrada_A, entrada_B, registro_F_anterior[0], control_ALU)
-    registro_F_acutal = logica_control_temporizacion(registro_F_anterior, banderas_actual, [0,0,0,0], control_LCT)
+    registro_F_acutal = logica_control_temporizacion(registro_F_anterior, banderas_actual, [0,0,0,0], [0,0,0,0,0,0], control_LCT)
 
     acumulador_A_actual = resultado_actual
 
